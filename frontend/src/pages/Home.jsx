3cdb62389f263f113a8eb5a0933266ef30059411
Syed Hisham Akmal <hisham0502@gmail.com>
2023-10-25 20:30:49 +0530

    Enhancing the UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
   const [activeFeature, setActiveFeature] = useState(0);
@@ -37,60 +38,43 @@ const Home = () => {
     setActiveFeature(index);
   };
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="min-h-screen bg-gradient-to-r from-purple-400 to-indigo-700 text-white p-8">
-      <header className="text-center mb-8">
-        <h1 className="text-5xl font-extrabold">Welcome to Meetify</h1>
-        <p className="text-2xl mt-4">Connect, Collaborate, Create Memories</p>
+    <div className="bg-gradient-to-r from-blue-400 to-indigo-600 text-white">
+    <Navbar />
+
+      <header className="py-16 text-center bg-gradient-to-r from-blue-500 to-indigo-700 animate-fade-in">
+        <h1 className="text-5xl font-extrabold mb-6">Welcome to Meetify</h1>
+        <p className="text-2xl">Connect, Collaborate, Create Memories</p>
       </header>
 
-      <section className="text-center mb-12">
-        <h2 className="text-3xl font-semibold">Features</h2>
-        <div className="relative mt-6">
-          {activeFeature > 0 && (
-            <button
-              className="absolute left-0 top-1/2 transform -translate-y-1/2 text-3xl text-white hover:text-indigo-300 transition transform hover:scale-110 focus:outline-none"
-              onClick={prevFeature}
-            >
-              &#9664;
-            </button>
-          )}
-          <div className="p-8 mx-4 bg-white bg-opacity-10 rounded-xl">
-            <div className="text-5xl mb-4">{features[activeFeature].icon}</div>
-            <h3 className="text-2xl font-semibold mb-2">{features[activeFeature].title}</h3>
-            <p className="text-gray-400">{features[activeFeature].description}</p>
+      <section className="text-center mb-12 p-6">
+        <h2 className="text-3xl font-semibold mb-4">Features</h2>
+        {features.map((feature, index) => (
+          <div key={index} className="mb-8">
+            <div className="text-5xl mb-4">{feature.icon}</div>
+            <h3 className="text-2xl font-semibold mb-2">{feature.title}</h3>
+            <p className="text-gray-400">{feature.description}</p>
           </div>
-          {activeFeature < features.length - 1 && (
-            <button
-              className="absolute right-0 top-1/2 transform -translate-y-1/2 text-3xl text-white hover:text-indigo-300 transition transform hover:scale-110 focus:outline-none"
-              onClick={nextFeature}
-            >
-              &#9654;
-            </button>
-          )}
-        </div>
-        <div className="mt-4 flex justify-center">
-          {features.map((_, index) => (
-            <button
-              key={index}
-              className={`text-xl mx-2 focus:outline-none ${
-                index === activeFeature
-                  ? 'text-indigo-500'
-                  : 'text-white hover:text-indigo-300'
-              }`}
-              onClick={() => goToFeature(index)}
-            >
-              &bull;
-            </button>
-          ))}
-        </div>
+        ))}
       </section>
 
-      <section className="text-center">
-        <h2 className="text-3xl font-semibold">Get Started</h2>
-        <p className="text-xl mt-4">Join Meetify and make your college life unforgettable!</p>
-        <button className="bg-indigo-600 text-white py-3 px-6 mt-6 text-lg rounded-full hover:bg-indigo-700 transition">Sign Up Now</button>
+      <section className="text-center py-16 bg-gradient-to-r from-blue-500 to-indigo-700">
+        <h2 className="text-3xl font-semibold mb-4">Get Started</h2>
+        <p className="text-xl mb-6">
+          Join Meetify to discover a world of possibilities on your campus. Connect with like-minded peers, collaborate on exciting projects, create lasting memories, and enjoy a personalized experience like never before.
+        </p>
+        <button className="bg-indigo-600 text-white py-3 px-6 text-lg rounded-full hover:bg-indigo-800 transition">Sign Up Now</button>
       </section>
+
+      <footer className="bg-indigo-800 p-8 text-center">
+        <p className="text-white">© 2023 Meetify. All rights reserved.</p>
+      </footer>
     </div>
   );
 };
